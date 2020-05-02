d26897fe0d131eb7e6eabb232422b92aaac139f6
Prashantha Gowda <prashantha.g@gmail.com>
2020-05-02 15:19:23 -0700

    * bashrc and nvm

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
-sudo cp  .vimrc .screenrc .tmux.conf ~/
-sudo curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+#!/bin/bash
+sudo -u ina chown $USER:$USER -R ~/
+cp  .bashrc .vimrc .screenrc .tmux.conf ~/
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+git clone https://github.com/nvm-sh/nvm.git ~/.nvm
+.  ~/.bashrc
+
