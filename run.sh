850595e0c156a946a1d82a178548883a74415920
ina <Prashantha Gowda>
2020-06-26 22:24:09 -0700

    *appending bashrc rather overwriting

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-sudo -u ina chown $USER:$USER -R ~/
-cp  .bashrc .vimrc .screenrc .tmux.conf ~/
+#sudo -u ina chown $USER:$USER -R ~/
+cp   .vimrc .screenrc .tmux.conf ~/
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 git clone https://github.com/nvm-sh/nvm.git ~/.nvm
 sudo yum install -y autossh
 sudo apt install -y autossh
+cat env >> ~/.bashrc
 .  ~/.bashrc
 
