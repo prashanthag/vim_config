050ad53091e4fe44f26b8f93de6ac60de63feb9e
Prashantha Gowda <prashantha.g@gmail.com>
2021-04-16 08:07:04 -0700

    * virtualenvwrapper

--- run.sh
+++ run.sh
@@ -6,8 +6,10 @@ cp   .vimrc .screenrc .tmux.conf ~/
 curl -fLo plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp plug.vim ~/.vim/autoload
 git clone https://github.com/nvm-sh/nvm.git ~/.nvm
-sudo yum install -y autossh
-sudo apt install -y autossh
+sudo yum install -y autossh python3-pip
+sudo apt install -y autossh python3-pip
+# mkvirtualenv               workon
+pip3 install virtualenvwrapper
 cat env >> ~/.bashrc
 .  ~/.bashrc
 
